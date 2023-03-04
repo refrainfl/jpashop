dfe0f30678b3be5fd79a95af5be277f0992cbdda
FL <refrain2k@gmail.com>
2023-03-04 19:46:56 +0900

    Day #4 MemberService Test Case 작성

--- src/test/java/com/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/com/jpabook/jpashop/service/MemberServiceTest.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+
 import static org.junit.Assert.*;
 
 
@@ -18,12 +20,11 @@ import static org.junit.Assert.*;
 public class MemberServiceTest {
 
     @Autowired MemberService memberService;
-    @Autowired
-    MemberRepository memberRepository;
-
+    @Autowired MemberRepository memberRepository;
+    @Autowired EntityManager em;
 
     @Test
-    public void 회원가입() throws Exception{
+    public void 회원가입() throws Exception {
         //given
         Member member = new Member();
         member.setName("Lee");
@@ -32,7 +33,22 @@ public class MemberServiceTest {
         Long savedId = memberService.join(member);
 
         //then
-        assertEquals(member,memberRepository.findOne(savedId));
+        assertEquals(member, memberRepository.findOne(savedId));
     }
 
+    @Test(expected = IllegalStateException.class)
+    public void 중복_회원_예외() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setName("Lee");
+        Member member2 = new Member();
+        member2.setName("Lee");
+
+        //when
+        memberService.join(member1);
+        memberService.join(member2); // 예외 발생
+
+        //then
+        fail("예외가 발생해야 한다.");
+    }
 }
